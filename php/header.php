aef8af582043635c6f8fc2a55a0f0f6e00c9d102
EVRARD <evrardisabel@hotmail.fr>
2025-06-18 16:20:36 +0200

    header

--- php/header.php
+++ php/header.php
@@ -1,40 +1,48 @@
 <link rel="stylesheet" href="../Public/css/header.css">
+<link rel="stylesheet" href="../Public/css/nav.css">
+<link rel="stylesheet" href="../Public/css/styles.css">
+<link rel="stylesheet" href="../Public/css/variables.css">
+<link rel="stylesheet" href="../Public/css/font.css">
+
 <script src="../JS/header.js"></script>
-<header>
+<nav>
+
+<header class="header flex1">
+
    <a href="accueil.php" class="logo">
                 <img src="../img/logo_POP_STREAMING.png" alt="Logo">
             </a>
-            
+       <div class="flex">
             <div class="recherche">
                 <form action="recherche.php" method="POST">
-                    <input type="search" id="film" name="film" placeholder="Rechercher un film, une série...">
-                    <button type="submit">Rechercher</button>
+                    <input class="btn-recherche" type="search" id="film" name="film" placeholder="Rechercher un film, une série..." alt="recherche">
+                    <button class="btn-recherche" type="submit">Rechercher</button>
                 </form>
             </div>
             
             <div class="nav-actions">
                 <button class="notification">
-                    <img src="" alt="Notifications">
+                    <img src="../img/bell.svg" alt="Notifications">
                 </button>
                 
                 <div class="langue">
-                    <button class="langue-selection">FR</button>
-                    <ul class="language-affichage">
-                        <li><a href="#">FR</a></li>
-                        <li><a href="#">EN</a></li>
-                        <li><a href="#">ES</a></li>
+                    <button class="langue-selection btn-FR">FR</button>
+                    <ul class="language-affichage ">
+                        <li><a class="btn-FR1" href="#">FR</a></li>
+                        <li><a class="btn-FR1" href="#">EN</a></li>
+                        <li><a class="btn-FR1" href="#">ES</a></li>
                     </ul>
                 </div>
                 
-                <a href="#" class="profile-link">
-                    <img src="" alt="Photo de profil" class="profile-image">
+                <a href="profils.php" class="profile-link">
+                    <img class="photo-profils" src="../img/defaut_profil.jpeg" alt="Photo de profil" class="profile-image">
                     <ul class="options-affichage">
-                        <li> <a href="#"> Mon compte</a> </li>
-                        <li> <a href="#"> Se déconnecter </a> </li>
+                        <li> <a class="p" href="modif_profils.php"> Mon compte</a> </li>
+                        <li> <a class="p" href="deconnexion.php"> Se déconnecter </a> </li>
                     </ul>
                 </a>
             </div>
-        </nav>
-
+       </div>
 
 </header>
+</nav>
