3476a5c85a43e0188078e32c2659c3f4e1517f28
EVRARD <evrardisabel@hotmail.fr>
2025-06-24 10:24:16 +0200

    Fichier ok Affiche Recherche

--- php/afficheRecherche.php
+++ php/afficheRecherche.php
@@ -66,56 +66,54 @@ $urlPrecedent .= "page=" . ($page - 1);
 
 <body>
 <?php require "nav_accueil.php";  ?>
-<a href="../php/pre_accueil.php">
-    <input class="chevron" type="image" src="../Public/img/btn-retour.png" alt="<"/></a> <br>
+    <a href="../php/pre_accueil.php">
+        <input class="chevron" type="image" src="../Public/img/btn-retour.png" alt="<"/></a> <br>
 
 <div class="flex">
 <?php require "nav.php"; // On inclut la barre de navigation ?>
 
     <!-- Affichage des contenus retrouvé -->
 
-    <div>
-        <form action="afficheRecherche.php" method="POST">
-            <p>Rechercher</p>
-            <label for="film">Recherche </label>
-            <input  class="btn-secondary" type="search" id="film" name="film" placeholder=<?php if (isset($mot)) echo $mot ?> "Rechercher un film, une série...">
-        </form>
-    </div>
+        <div class="search-box">
+                <form action="afficheRecherche.php" method="POST">
+                    <p>Rechercher</p>
+                    <label for="film">Recherche </label>
+                    <input  class="btn-secondary" type="search" id="film" name="film" placeholder=<?php if (isset($mot)) echo $mot ?> "Rechercher un film, une série...">
+                </form>
+        </div>
 
+        <div class="img">
+            <?php
+            $init = (12*$page);
+            $total = 0;
 
-<div>
-    <?php
-    $init = (12*$page);
-    $total = 0;
+                for ($i=0; $i <=1; $i++) { // 2 lignes
+                for ($j=0; $j < 6 ; $j++) {
 
-    for ($i=0; $i <=1; $i++) { // 2 lignes
-        for ($j=0; $j < 6 ; $j++) {
-
-            if (isset($resultat[$init]['poster_path'])) {
-                $img = $resultat[$init]['poster_path'];
-                echo "<a href=\"\"><img src='$img' width='200' height='200' alt=''> </a>" ;
-                $total++;
+                    if (isset($resultat[$init]['poster_path'])) {
+                        $img = $resultat[$init]['poster_path'];
+                        echo "<a href=\"\"><img src='$img' width='200' height='200' alt=''> </a>" ;
+                        $total++;
+                    }
+                    $init++;
+                } echo "<br>";
             }
-            $init++;
-        } echo "<br>";
-    }
-    $end = !(($total == 12 && isset($resultat[$init]))); // Si on a pas 12 ou que le suivant n'est pas
-    //défini , on est a la fin de la liste
+            $end = !(($total == 12 && isset($resultat[$init]))); // Si on a pas 12 ou que le suivant n'est pas
+            //défini , on est a la fin de la liste
 
+            ?>
+        </div>
 
-
-    ?>
-    <?php if (isset($_GET['page']) && $_GET['page'] > 0): ?>
-        <a class href="<?php echo $urlPrecedent?>"> Page précedente </a>
-    <?php endif; ?>
-    <?php if (!$end): ?>
-        <a href="<?php echo $urlSuivant?>"> Page suivante </a>
-    <?php endif; ?>
+        <div class="flex espacement">
+        <?php if (isset($_GET['page']) && $_GET['page'] > 0): ?>
+            <a class='pagination' href="<?php echo $urlPrecedent?>"> Page précedente </a>
+        <?php endif; ?>
+        <?php if (!$end): ?>
+        <a class='pagination' href="<?php echo $urlSuivant?>"> Page suivante </a>
+        <?php endif; ?>
+        </div>
 
 </div>
-</div>
-
-
 
 
 
